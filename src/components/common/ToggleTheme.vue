e14bd2084a20aec4ae61beb3484080607814d70f
zclzone <zclzone@outlook.com>
2024-06-30 16:58:57 +0800

    style: 删除无用注释代码

--- src/components/common/ToggleTheme.vue
+++ src/components/common/ToggleTheme.vue
@@ -40,23 +40,5 @@ async function toggleDark({ clientX, clientY }) {
       pseudoElement: `::view-transition-${isDark.value ? 'old' : 'new'}(root)`,
     },
   )
-
-  // const maxRadius = Math.hypot(
-  //   Math.max(clientX, window.innerWidth - clientX),
-  //   Math.max(clientY, window.innerHeight - clientY),
-  // )
-  // const style = document.documentElement.style
-  // style.setProperty('--circle-x', `${clientX}px`)
-  // style.setProperty('--circle-y', `${clientY}px`)
-  // style.setProperty('--circle-r', `${maxRadius}px`)
-
-  // function handler() {
-  //   appStore.toggleDark()
-  //   useToggle(isDark)()
-  // }
-
-  // document.startViewTransition
-  //   ? document.startViewTransition(handler)
-  //   : handler()
 }
 </script>
